d2127996049ddf2385279ac4cebfba059717fc7d
Nicolas Mondon <nicolasmondon@gmail.com>
2020-06-12 13:49:27 +0200

    v2: create Fulgur file

--- src/v2/Fulgur.tsx
+++ src/v2/Fulgur.tsx
@@ -1,1 +1,17 @@
-import React from 'react';
+import { scaleLinear } from 'd3-scale';
+import _ from 'lodash/fp';
+import * as React from 'react';
+
+const Fulgur = React.memo((props: any) => {
+    const {
+        children,
+        xScale = scaleLinear,
+        yScale = scaleLinear,
+        origin = ['left', 'bottom'],
+        x = _.get('x'),
+        y = _.get('y'),
+        ratio = 1
+    } = props;
+
+    return <></>;
+});
